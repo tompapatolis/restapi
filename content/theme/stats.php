7507969b9f21e66d8e01710c9c359304f3cdb78e
tomsnews <49147550+tomsnews@users.noreply.github.com>
2019-04-07 19:55:49 +0200

    Updated Buttons in Framework

--- content/theme/stats.php
+++ content/theme/stats.php
@@ -1,7 +1,36 @@
 <section class="section">
 	<div class="container">
-		<button class="btn red">Button</button>
 
+		<h1>Colors</h1>
+		<button class="btn red">Red</button>
+		<button class="btn blue">Blue</button>
+		<button class="btn green">Green</button>
+		<button class="btn yellow">Yellow</button>
+		<button class="btn gray">Gray</button>
+		<button class="btn white">White</button>
+		<button class="btn outline">Outline</button>
+
+		<br><br>
+		
+		<h1>Button Group</h1>
+		<div class="btn-group">
+			<button class="btn red">Red</button>
+			<button class="btn blue">Blue</button>
+			<button class="btn green">Green</button>
+			<button class="btn yellow">Yellow</button>
+			<button class="btn gray">Gray</button>
+			<button class="btn white">White</button>
+			<button class="btn outline">Outline</button>
+		</div>
+
+		<br>
+
+		<h1>Sizes</h1>
+		<button class="btn red btn-tiny">Tiny</button>
+		<button class="btn blue btn-small">Small</button>
+		<button class="btn green">Default</button>
+		<button class="btn yellow btn-large">Large</button>
+		<button class="btn gray btn-huge">Huge</button>
 		
 	</div>
 </section>
