d4559cee03cdf430150fb7e7f8085a944fbeb123
tomsnews <49147550+tomsnews@users.noreply.github.com>
2019-04-08 09:03:19 +0200

    More Button Framework updates

--- content/theme/stats.php
+++ content/theme/stats.php
@@ -12,7 +12,8 @@
 
 		<br><br>
 		
-		<h1>Button Group</h1>
+		<h1>Button Groups</h1>
+		<h3>Simple</h3>
 		<div class="btn-group">
 			<button class="btn red">Red</button>
 			<button class="btn blue">Blue</button>
@@ -25,12 +26,42 @@
 
 		<br>
 
+		<h3>Block</h3>
+		<div class="btn-group btn-block">
+			<button class="btn red">Red</button>
+			<button class="btn blue">Blue</button>
+			<button class="btn green">Green</button>
+			<button class="btn yellow">Yellow</button>
+			<button class="btn gray">Gray</button>
+			<button class="btn white">White</button>
+			<button class="btn outline">Outline</button>
+		</div>		
+
+		<br>
+
 		<h1>Sizes</h1>
 		<button class="btn red btn-tiny">Tiny</button>
 		<button class="btn blue btn-small">Small</button>
 		<button class="btn green">Default</button>
 		<button class="btn yellow btn-large">Large</button>
 		<button class="btn gray btn-huge">Huge</button>
+		<br><br>
+		<button class="btn white btn-block">Block</button>
+
+		<br>
+
+		<h1>Position</h1>
+		<button class="btn red">Left</button>
+		<button class="btn blue btn-center">Center</button>
+		<button class="btn green btn-right">Right</button>
+
+		<br>
+
+		<h1>Round Style</h1>
+		<button class="btn red btn-round"><i class="vi vi-cloud-upload"></i></button>		
+		<button class="btn blue btn-round"><i class="vi vi-link"></i></button>
+		<button class="btn green btn-round"><i class="vi vi-menu"></i></button>
+		<button class="btn gray btn-round"><i class="vi vi-phone-handset"></i></button>
 		
 	</div>
 </section>
